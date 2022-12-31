adfbc7bc62b178eec640767393b3a353491d0ff2
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-12-31 23:18:52 +0900

    u.c. switch to m1

--- bb/reports.clj
+++ bb/reports.clj
@@ -29,18 +29,13 @@
 (defn curl-get [url & params]
   (curl/get url {:raw-args (vec (concat ["-b" cookie] params))}))
 
-(defn curl-post [url & params]
-  (curl/post url {:raw-args (vec (concat ["-b" cookie] params))}))
+;; (defn curl-post [url & params]
+;;   (curl/post url {:raw-args (vec (concat ["-b" cookie] params))}))
 
 (defn today []
   (-> (sh "date" "+%F")
       :out
       str/trim-newline))
-
-(defn date-before-today
-  "return today - n date"
-  [n]
-  ())
 
 (comment
   (today)
@@ -69,12 +64,11 @@
       (filter :valid ret)
       ret)))
 
-(reset! users (fetch-users true))
-
 (comment
   users
   :rcf)
 
+(reset! users (fetch-users true))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; fetch meas
@@ -84,24 +78,24 @@
   (log/info "fetch-meas" id type since)
   (mysql/execute!
    db
-   ["select measure, created from meas
+   ["select measure from meas
      where user_id=? and type=? and created >?"
     id type since]))
 
 (defn fetch-meas-before
+  "returns `id` measure `type` between today and `days` before."
   [id type days]
   (mysql/execute!
    db
-   ["select measure from meas
-     where user_id=? and type=? and
-           created > current_timestamp - interval ? day"
+   ["select measure, created from meas
+     where user_id=? and
+           type=? and
+           created > current_timestamp - interval ? day
+     order by created"
     id type days]))
 
 (comment
   (fetch-meas 51 1 "2022-12-10")
   (fetch-meas 16 1 "2022-11-20")
-  (fetch-meas-before 51 1 20)
+  (fetch-meas-before 16 1 75)
   :rcf)
-;; (defn weights
-;;   ""
-;;   [id since])
