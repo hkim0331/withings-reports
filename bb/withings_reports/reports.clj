83620fa241fe22c0ae9baeac1d121fba4b0f89f3
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-01-24 12:32:41 +0900

    updated on m64

--- bb/withings_reports/reports.clj
+++ bb/withings_reports/reports.clj
@@ -135,6 +135,9 @@
 (def hkimura (-> (filter #(= "hkimura" (:name %)) @users)
                  first))
 
+(def ishii (-> (filter #(= "ishii" (:name %)) @users)
+                 first))
+ishii
 ;; (def saga-user (-> (filter #(= 51 (:id %)) @users)
 ;;                    first))
 
@@ -384,7 +387,7 @@
             (log/info "reports error:" (.getMessage e))))))))
 
 (comment
-  (reports [hkimura] [1 76 77] [1 7 28] [25 75] :debug)
+  (reports [ishii] [1 76 77] [1 7 28] [25 75] :debug)
   (reports [hkimura] [1 76 77] [10 7 28] [25 75])
   :rcf)
 
